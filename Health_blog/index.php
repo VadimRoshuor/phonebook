64cb5c0f7e276d3b873ac3977c6c7b3e46333e59
RosVad <rovd_00@ex.ua>
2017-04-06 15:35:08 +0300

    sait for WorkWave

---
+++ Health_blog/index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>YH</title>
+
+	<!--JQuery-->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+	<!--Font Awesome-->
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+
+	<!--Main styles-->
+	<link rel="stylesheet" href="styles/style.css">
+
+</head>
+<body>
+<!--<div class="container-fluid navbar-fixed-top">-->
+<div class="container-fluid ">
+	<div class="row social_menu">
+		<div class="col-lg-10 col-md-8 col-sm-0 col-xs-0"></div>
+		<div class="col-lg-2 col-md-4 col-sm-12 col-xs-12 social_links">
+			<a href="#"><i class="fa fa-vk fa-3x" aria-hidden="true"></i></a>
+			<a href="#"><i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i></a>
+			<a href="#"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid main_menu_block ">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#resp_menu">
+				<!--<span class="sr-only">Toggle navigation</span>-->
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand logo_YH" href="#"><h1>Your health</h1>
+				<h3>Твое здоровье в твоих руках</h3></a>
+		</div>
+
+		<div class="collapse navbar-collapse " id="resp_menu">
+
+			<ul class="nav navbar-nav main_menu">
+				<li class="active"><a href="#">Главная<span class="sr-only">(current)</span></a></li>
+				<li><a href="#">Фитнес</a></li>
+				<li><a href="#">Питание</a></li>
+				<li><a href="#">О блоге</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<!--</div>-->
+<div class="container">
+	<div class="row">
+
+		<div class="col-lg-12  main_cont">
+
+			<div class="row">
+				<div class="col-sm-6 col-md-4 col-lg-4">
+					<div class="thumbnail title_post">
+						<a href=""><img src="http://cdn-media-2.lifehack.org/wp-content/files/2016/08/11144845/23705012302_5ae6e24beb_k-370x208.jpg" alt="..."></a>
+						<div class="caption">
+							<a href=""><h3>заголовок о здоровье или еще о какой-то хуйне</h3></a>
+							<p>Сокращенный текст статьи о здоровье или о всякой подобной лабуде</p>
+							<p><a href="#" class="btn" role="button">Подробнее</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4 col-lg-4">
+					<div class="thumbnail">
+						<a href=""><img src="http://cdn-media-1.lifehack.org/wp-content/files/2016/08/11225558/photo-1453227588063-bb302b62f50b-370x208.jpg" alt="..."></a>
+						<div class="caption">
+							<a href=""><h3>заголовок о здоровье или еще о какой-то хуйне</h3></a>
+							<p>Сокращенный текст статьи о здоровье или о всякой подобной лабуде</p>
+							<p><a href="#" class="btn " role="button">Подробнее</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4 col-lg-4">
+					<div class="thumbnail">
+						<a href=""><img src="http://cdn-media-2.lifehack.org/wp-content/files/2016/08/09223401/graduation-879941_960_720-370x208.jpg" alt="..."></a>
+						<div class="caption">
+							<a href=""><h3>заголовок о здоровье или еще о какой-то хуйне</h3></a>
+							<p>Сокращенный текст статьи о здоровье или о всякой подобной лабуде</p>
+							<p><a href="#" class="btn" role="button">Подробнее</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4 col-lg-4">
+					<div class="thumbnail">
+						<a href=""><img src="http://cdn-media-1.lifehack.org/wp-content/files/2016/08/09162730/coffee-desk-notes-workspace-370x208.jpg" alt="..."></a>
+						<div class="caption">
+							<a href=""><h3>заголовок о здоровье или еще о какой-то хуйне</h3></a>
+							<p>Сокращенный текст статьи о здоровье или о всякой подобной лабуде</p>
+							<p><a href="#" class="btn" role="button">Подробнее</a></p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-12 pagination_block">
+					<nav aria-label="Page navigation">
+						<ul class="pagination">
+							<li>
+								<a href="#" aria-label="Previous">
+									<span aria-hidden="true">&laquo;</span>
+								</a>
+							</li>
+							<li><a href="#">1</a></li>
+							<li><a href="#">2</a></li>
+							<li><a href="#">3</a></li>
+							<li><a href="#">4</a></li>
+							<li><a href="#">5</a></li>
+							<li>
+								<a href="#" aria-label="Next">
+									<span aria-hidden="true">&raquo;</span>
+								</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+
+</div>
+
+<footer class="footer">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-12">
+
+				© 2016 <a href="#" class="footer_logo">YH</a>
+				<br>
+				Копирование информации только с ссылкой на этот ресурс
+				<br>
+				<a href="#"><i class="fa fa-vk fa-3x" aria-hidden="true"></i></a>
+				<a href="#"><i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i></a>
+				<a href="#"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>
+
+			</div>
+		</div>
+	</div>
+</footer>
+
+
+</body>
+</html>
